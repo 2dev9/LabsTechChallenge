286a78b51700a429d1e6bcea56b536006fd4afbb
howell.devon@gmail.com <howell.devon@gmail.com>
2018-01-17 16:28:49 -0500

    Added the initial HTML

--- app.py
+++ app.py
@@ -7,6 +7,9 @@ app = Flask(__name__)
 def main():
     return render_template('index.html')
 
+@app.route('/dah2184', methods=['GET'])
+def main():
+    return render_template('devon.html')
 
 if __name__ == '__main__':
     app.run()
